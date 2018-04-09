477752985e405f12b22aa0899f8296a808ba4538
Jos van Goor <josvangoor@gmail.com>
2018-04-10 00:19:47 +0200

    Youtube jwz

--- src/modules/YoutubeModule.py
+++ src/modules/YoutubeModule.py
@@ -1,9 +1,15 @@
-import discord
 import asyncio
+import discord
+import re
 import youtube_dl
 import modules.ModuleBase as base
 
 class YoutubeModule(base.ModuleBase):
+    STATE_IDLE = 0      # not playing anything
+    STATE_PLAYING = 1   # playing a song
+    STATE_STARTING = 2  # starting up a song
+    STATE_STOPPING = 3  # cleaning up
+
 
     def __init__(self, client):
         super().__init__(client)
@@ -11,6 +17,8 @@ class YoutubeModule(base.ModuleBase):
         self.player = None
         self.voice = None
         self.channel = None
+        self.song = ""
+        self.state = self.STATE_IDLE
         self.timer = 0
 
         print('YoutubeModule initialized...')
@@ -22,7 +30,25 @@ class YoutubeModule(base.ModuleBase):
     # This method gets called when a command arrives that passed this module's filter
     # This function can return a string which will be the bot's response.
     async def handle_message(self, message):
-        
+        if not message.channel.name == 'botspam': return
+        args = message.content.split(' ')
+
+        if len(args) == 2:
+            if args[1] == 'stop':
+                self.queue = []
+                self.state = self.STATE_STOPPING
+                return
+
+            if args[1] == 'next':
+                self.state = self.STATE_STARTING
+                return
+
+            # it must be a link then, start playin bojj
+            self.queue.append(args[1])
+            self.channel = message.channel
+            if self.state == self.STATE_IDLE: # not if were busy tho
+                self.state = self.STATE_STARTING
+            return
 
         await super().handle_message(message)
 
@@ -53,4 +79,66 @@ class YoutubeModule(base.ModuleBase):
 
     # This method gets called once every second for time based operations.
     async def update(self):
-        pass
+        # not doing anything, return
+        if self.state == self.STATE_IDLE:
+            return
+
+        # update timer and go to starting if song ended
+        if self.state == self.STATE_PLAYING:
+            self.timer -= 1
+            if self.timer == 0:
+                self.state = self.STATE_STARTING
+            return
+        
+        # clean resources.
+        if self.state == self.STATE_STOPPING:
+            self.queue = []
+            self.song = ""
+            self.state = self.STATE_IDLE
+
+            if(self.player):
+                self.player.stop()
+                self.player = None
+            if(self.voice):
+                await self.voice.disconnect()
+                self.voice = None
+            
+            return
+
+        # somebody said music?
+        if self.state == self.STATE_STARTING:
+            # No! =(
+            if len(self.queue) == 0:
+                self.state = self.STATE_STOPPING
+                return
+            
+            # clean this old shit up
+            if self.player:
+                self.player.stop()
+                self.player = None
+
+            song = self.queue.pop()
+            if re.match(r'^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+', song):
+                try:
+                    if not self.voice:
+                        self.voice = await self.client.join_voice_channel(self.channel)
+                    
+                    self.player = await self.voice.create_ytdl_player(song)
+                    if(self.player.is_live): self.timer = -1
+                    else: self.timer = self.player.duration + 2
+
+                    self.player.start()
+
+                    self.state = STATE_PLAYING
+                except discord.ClientException as e:
+                    print(e)
+                    self.state = self.STATE_STOPPING
+                except youtube_dl.utils.DownloadError as e:
+                    print(e)
+                    self.state = self.STATE_STOPPING
+
+            else:
+                await self.client.send_message(message.channel, 'That is not an youtube url you cheecky bastard :)')
+
+
+
